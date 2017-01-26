c7756c3364c1571559627e38d04c9960ce6a9310
Manish Jain <jainmanish01995@gmail.com>
2017-01-26 21:09:42 +0530

    Cosine similarity - frequency vector for word 1

--- Project/src/soft/Cosine.java
+++ Project/src/soft/Cosine.java
@@ -9,7 +9,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,22 +22,63 @@ import java.util.stream.Collectors;
  * @author Manish
  */
 public class Cosine {
-  
-void Cosine_score(File f1,File f2) throws Exception
+   public class Cos
+ {
+  int wd1;
+  int wd2;
+  Cos(int v1, int v2)
+  {
+   this.wd1=v1;
+   this.wd2=v2;
+  }
+  public void Update_VAl(int v1, int v2)
+  {
+   this.wd1=v1;
+   this.wd2=v2;
+  }
+ }
+double Cosine_score(File f1,File f2) throws Exception
    {
-
+       double sim_score=0.0000000;
+ Hashtable<String, Cos> frequency_wordv = new Hashtable<String, Cosine.Cos>();
+  LinkedList<String> Distinct_words_text_1_2 = new LinkedList<String>();
 		BufferedReader br1 = new BufferedReader(new FileReader(f1));
 		BufferedReader br2 = new BufferedReader(new FileReader(f2));
 
 		List<String> l1 = br1.lines().collect(Collectors.toList());
 		List<String> l2 = br2.lines().collect(Collectors.toList());
-               String [] countries = l1.toArray(new String[l1.size()]); 
+                
+                for (String sim : l1 ){
+                   String simi[] =sim.split("\\s");  
+                    for(int i=0;i<simi.length;i++)
+  {
+   String tmp_wd = simi[i].trim();
+   if(tmp_wd.length()>0)
+   {
+    if(frequency_wordv.containsKey(tmp_wd))
+    {
+     Cos vals1 = frequency_wordv.get(tmp_wd);
+     int freq1 = vals1.wd1+1;
+     int freq2 = vals1.wd2;
+     vals1.Update_VAl(freq1, freq2);
+     frequency_wordv.put(tmp_wd, vals1);
+    }
+    else
+    {
+     Cos vals1 = new Cos(1, 0);
+     frequency_wordv.put(tmp_wd, vals1);
+     Distinct_words_text_1_2.add(tmp_wd);
+    }
+   }
+  }
+                }
+                       
+  return(sim_score);
+   
+                
+                
 
-               for(String s:countries)
-               {
-                     
-                   System.out.println(s);
-               }
+              
            //     for (String s:l1)
             //    {
              /*   String pattern="[\\s]";
@@ -47,22 +90,7 @@ void Cosine_score(File f1,File f2) throws Exception
                 // System.out.println(str);
            //     System.out.println(l2);
                 //}
-                
-                
-                Iterator<String> itr1 = l1.iterator();
-		Iterator<String> itr2 = l2.iterator();
-                
-                System.out.println(itr1);
-                System.out.println(itr2);
-                
-        double VectAB = 0.0000000;
-       double VectA_Sq = 0.0000000;
-         double VectB_Sq = 0.0000000;
-       
-
-        
-      
-       
+ 
    }
     
     
@@ -73,8 +101,9 @@ void Cosine_score(File f1,File f2) throws Exception
     File f2=new File("C:\\Users\\Manish\\Desktop\\Project\\file2.java");
       Cosine cs1 = new Cosine();
     //double Cos_sim_score= cs1.Cosine_score("My project is Software plagiarism", "My project is Software Plagiarism");
-    cs1.Cosine_score(f1,f2);   
-    //System.out.println("the Cosine Similarity Score is " +Cos_sim_score);
+   System.out.println("[Word # VectorA # VectorB]");
+    double sim_score =cs1.Cosine_score(f1,f2);   
+    System.out.println("the Cosine Similarity Score is " +sim_score);
     }
    
     
