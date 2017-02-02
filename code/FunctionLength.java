5b0015d82d699254ae70d55d73333136d9fffb4b
kunal naik <naikkunal11@gmail.com>
2017-02-02 19:58:28 +0530

    function length calculate

--- code/FunctionLength.java
+++ code/FunctionLength.java
@@ -4,15 +4,19 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
-public class FunctionLength {
-private static void Functionlength(String File) throws FileNotFoundException, IOException {
 
-int totalCharacters = 0;
-                int functioncount=0;
+public class FunctionLength {
+
+    private static void Functionlength(String File) throws FileNotFoundException, IOException {
+ 
+		
+		int totalCharacters = 0;
+    int functioncount=0;
            
 		String str;
 
 		try (BufferedReader br = new BufferedReader(new FileReader(File))){
+		
                 while ((str = br.readLine()) != null){
                    
                    if(str.contains("private void")||str.contains("public int")||(str.contains("void"))||(str.contains("private int"))){
@@ -21,34 +25,32 @@ int totalCharacters = 0;
                        
                        System.out.println(str);
                 String[] str1 = str.replaceAll("\\s+", " ").replaceAll("\\W", "").split(" ");
-
-
-
-}
-
-}
-
-
-
-
- catch (IOException e) {
-			e.printStackTrace();
+               
+                for (String s : str1) {
+                
+					totalCharacters += s.length();
+				}
+            
+                }
+                
+                }
+               
+                }
+                 catch (IOException e) {
+          e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-
-public static void main(String[] args) throws IOException {
+                
+              System.out.println("totalCharacters:"+totalCharacters);
+            //  System.out.println("functioncount :"+functioncount);
+            
+    }
+    public static void main(String[] args) throws IOException {
        
         try {
 			String File = "C:\\Users\\kunal\\Desktop\\project\\code\\File1.java";
 			Functionlength(File);
-		
-		} 
-		
-		catch (FileNotFoundException e) {
+		}
+		 catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
     }
