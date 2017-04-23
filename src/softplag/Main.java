bf858195c6b581d4289872024cead2800432b507
kunal naik <naikkunal11@gmail.com>
2017-04-24 00:19:46 +0530

    Phase one updated with gui

---
+++ src/softplag/Main.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softplag;
+
+import static java.lang.Float.max;
+
+
+
+/**
+ *
+ * @author kunal
+ */
+public class Main {
+ 
+    public static void main(String[] args) {
+       
+    
+      STYCalculator.main(args);
+      
+//        System.out.println("File 1=");
+//        System.out.println("Layout value ="+STYCalculator.getWMLayoutValue());
+//        System.out.println("Style value ="+STYCalculator.getWMStyleValue());
+//        System.out.println("Structure ="+STYCalculator.getWMStructure());
+//        
+//        
+//        STYCalculator1.main(args);
+//        System.out.println("File 2=");
+//        System.out.println("Layout value ="+STYCalculator1.getWMLayoutValue());
+//        System.out.println("Style value ="+STYCalculator1.getWMStyleValue());
+//        System.out.println("Structure value ="+STYCalculator1.getWMStructure());
+//        
+//        float final1=(STYCalculator.getWMLayoutValue()+STYCalculator.getWMStyleValue()+STYCalculator.getWMStructure())/3;
+//        float final2=(STYCalculator1.getWMLayoutValue()+STYCalculator1.getWMStyleValue()+STYCalculator1.getWMStructure())/3;
+//        System.out.println();
+//        float f=(1-(Math.abs(STYCalculator.getWMLayoutValue()-STYCalculator1.getWMLayoutValue())/max(STYCalculator.getWMLayoutValue() , STYCalculator1.getWMLayoutValue())))*100;
+//        System.out.println("Files match according to programming Layout="+f);
+//        System.out.println();
+//        float f1=(1-(Math.abs(STYCalculator.getWMStyleValue()-STYCalculator1.getWMStyleValue())/(max(STYCalculator.getWMStyleValue(),STYCalculator1.getWMStyleValue()))))*100;
+//        System.out.println("Files match according to programming Style="+f1);
+//        System.out.println();
+//        float f2=(1-(Math.abs(STYCalculator.getWMStructure()-STYCalculator1.getWMStructure())/(max(STYCalculator.getWMStructure(),STYCalculator1.getWMStructure()))))*100;
+//        System.out.println("Files match according to programming structure="+f2);
+//        
+//        System.out.println();
+//        
+//        float out=(1-((Math.abs(final1-final2))/(max(final1,final2))))*100;
+//        System.out.println("Files are "+out+" percent similar");
+    }   
+}
